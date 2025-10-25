2f015d36957cf86912394c051274d7af9c71f92f
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2025-10-24 23:52:44 -0300

    disable quit confirmation

--- lua/custom/plugins/confirm-quit.lua
+++ lua/custom/plugins/confirm-quit.lua
@@ -1,7 +1,8 @@
-return {
+-- NOTE: Disabled for now, since I've gotten used to quitting without confirmation.
+return {} --[[{
   'yutkat/confirm-quit.nvim',
   event = 'CmdlineEnter',
   opts = {
     quit_message = 'Are you sure you want to close NeoVim?',
   },
-}
+}]]
