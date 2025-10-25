70399a7a4af858784ea56f0ec57b52ce581acb01
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2025-10-24 23:35:16 -0300

    add github copilot plugin

---
+++ lua/custom/plugins/github-copilot.lua
@@ -0,0 +1,73 @@
+return {
+  'zbirenbaum/copilot.lua',
+  dependencies = { 'copilotlsp-nvim/copilot-lsp' },
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  lazy = true,
+  opts = {
+    filetypes = {
+      ['*'] = false,
+      c = true,
+      c_sharp = true,
+      cpp = true,
+      css = true,
+      dart = true,
+      dockerfile = true,
+      go = true,
+      html = true,
+      java = true,
+      javascript = true,
+      lua = true,
+      pas = true,
+      pascal = true,
+      powershell = true,
+      python = true,
+      vim = true,
+    },
+    panel = {
+      enabled = true,
+      auto_refresh = false,
+      keymap = {
+        jump_prev = 'gp',
+        jump_next = 'gn',
+        accept = '<CR>',
+        refresh = 'gr',
+        open = '<M-CR>',
+      },
+      layout = {
+        position = 'right', -- | top | left | right | bottom |
+        ratio = 0.4,
+      },
+    },
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      hide_during_completion = true,
+      debounce = 75,
+      trigger_on_accept = true,
+      keymap = {
+        accept = '<M-y>',
+        accept_word = '<M-l>',
+        accept_line = false,
+        next = '<M-]>',
+        prev = '<M-[>',
+        dismiss = '<C-]>',
+      },
+    },
+    nes = {
+      enabled = true, -- requires copilot-lsp as a dependency
+      auto_trigger = true,
+      keymap = {
+        accept_and_goto = '<M-k>',
+        accept = '<M-j>',
+        dismiss = '<M-h>',
+      },
+    },
+    auth_provider_url = nil, -- URL to authentication provider, if not "https://github.com/"
+    copilot_node_command = 'node', -- Node.js version must be > 22
+    workspace_folders = {},
+    copilot_model = '',
+    disable_limit_reached_message = false, -- Set to `true` to suppress completion limit reached popup
+    root_dir = nil,
+  },
+}
